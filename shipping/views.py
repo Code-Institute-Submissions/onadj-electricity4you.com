96168d76ae87206aa69cebd51767bab00dc5acb7
onadj <oliver.nad@gmail.com>
2020-09-17 17:22:09 +0000

    added shipping delivery cost views and templates

--- shipping/views.py
+++ shipping/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Deliverycost
 
 # Create your views here.
+
+def all_deliverycost(request):
+    """ A view to show all delivery cost """
+
+    deliverycost = Deliverycost.objects.all()
+
+    context = {
+        'deliverycost' : deliverycost, 
+    }
+
+    return render(request, 'shipping/shipping.html', context)
