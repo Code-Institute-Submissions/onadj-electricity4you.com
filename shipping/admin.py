759ad21fa9f36fcbbced7299c13b4716d63f47f0
onadj <oliver.nad@gmail.com>
2020-09-17 15:07:06 +0000

    added product app shipping, models and fixtures

---
+++ shipping/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Deliverycost, Country
+
+# Register your models here.
+
+class DeliverycostAdmin(admin.ModelAdmin):
+    list_display = (
+        'country',
+        'sku',
+        'name',
+        'description',
+        'price',
+    )
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Deliverycost, DeliverycostAdmin)
+admin.site.register(Country, CountryAdmin)
---
+++ shipping/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Country(models.Model):
+    
+    name = models.CharField(max_length=50)
+    friendly_name = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Deliverycost(models.Model):
+    country = models.ForeignKey('Country', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
