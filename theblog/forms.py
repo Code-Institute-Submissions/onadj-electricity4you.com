3041c7f36c6bc30b52751819b251ddf0c4ddc463
onadj <oliver.nad@gmail.com>
2020-09-25 17:12:57 +0000

    blog app - added update/edit post functionality, updated forms in blog app with new form EditForm and made update_post.html template and styled it

--- theblog/forms.py
+++ theblog/forms.py
@@ -11,3 +11,14 @@ class PostForm(forms.ModelForm):
 		'author': forms.Select(attrs={'class': 'form-control'}),
 		'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': "Body"}),
 		}
+
+
+class EditForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		fields = ('title','body')
+		widgets = {
+		'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Ovdje mogu napisati sto hocu u placeholderu'}),
+		'author': forms.Select(attrs={'class': 'form-control'}),
+		'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': "Body"}),
+		}
