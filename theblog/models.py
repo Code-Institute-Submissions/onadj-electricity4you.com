15dfd7b077ff3ca0a9485ba5fa104ab79623fe31
onadj <oliver.nad@gmail.com>
2020-09-28 16:17:54 +0000

    added date_added =models.DateTime.Field in models.py

--- theblog/models.py
+++ theblog/models.py
@@ -17,6 +17,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
     category = models.CharField(max_length=255, default='uncategorized')
     likes = models.ManyToManyField(User, related_name='blog_posts')
 
