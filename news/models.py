2089a326fc8a1ca098fe0402198a9f7e7ddb28e3
onadj <oliver.nad@gmail.com>
2020-10-06 11:17:12 +0000

    added functionality to add images to  news page

--- news/models.py
+++ news/models.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 class AddNews(models.Model):
     title = models.CharField(max_length=255)
     #image_url = models.URLField(max_length=1024, null=True, blank=True)
-    #image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to="media/")
     body = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post_date = models.DateField(auto_now_add=True)
