3b3082b73a73a8a85ee75e328a8b1536eccb53d2
onadj <oliver.nad@gmail.com>
2020-10-14 16:42:39 +0000

    added secret key in heroku for django

--- electricity4you/settings.py
+++ electricity4you/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6nzdn)t0^1-_@hk&kf_hzhxn%ntj0^lndrvrhu%@u44c$72tfe'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['electricity4you.herokuapp.com', 'localhost']
 
