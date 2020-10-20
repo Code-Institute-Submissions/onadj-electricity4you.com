23b6c612140cc7d6778dfd3603a5d37b28233842
onadj <oliver.nad@gmail.com>
2020-10-20 10:18:53 +0000

    finalizing site with few visual changes and testing blog and news functionality

--- blog/forms.py
+++ blog/forms.py
@@ -7,7 +7,7 @@ class PostForm(forms.ModelForm):
 		fields = ('title', 'author', 'body')
 		widgets = {
 		'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Give Your Post Name'}),
-        'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'elder', 'type': 'hidden'}),
+        'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'onagy', 'type': 'hidden'}),
 		'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': "Write Your Post"}),
 		}
 
